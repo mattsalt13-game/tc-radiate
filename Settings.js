1c131d5453cc59453394df908809fa4218aab4ec
mattsalt13-game <52068222+mattsalt13-game@users.noreply.github.com>
2019-06-21 09:55:13 +0100

    Update Settings.js

--- Settings.js
+++ Settings.js
@@ -1,6 +1,6 @@
 ﻿var organizationSettings = {
     //The url that points to team city
-    teamCityUrl: 'https://todo.edit.this.url.invalid',
+    teamCityUrl: 'http:/Shfteamcity',
 
     //Only show builds for branches that satisfy the predicate
     branchFilter: function(branch) {
